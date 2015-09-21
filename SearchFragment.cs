3c86a5c9e006f414bbfe74964b76f6bade49502a
ktsolev <ktsolev@gmail.com>
2015-09-21 04:41:45 +0200

    Now Map Displays Current Position and Fixed bug when onChange Location Map Constantly Redraws itself

--- SearchFragment.cs
+++ SearchFragment.cs
@@ -12,31 +12,24 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Android.Locations;
 
 
 namespace EmergencyApp_v2
 {
 	public class SearchFragment : Fragment
-	{   private MapView MapView;
+	{   
+		private MapView MapView;
 		private GoogleMap Map;
+		private Location MyLocation;
+		private Location MyLastLocation;
 		private Marker NmtMarker;
-		private TextView CurrentLocation;
-
-
-		static readonly string Tag = "Coordinates";
+		private LatLng Lat;
 
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			var view = inflater.Inflate(Resource.Layout.SearchFragmentLayout, null);
-			TextView CurrentLocation = (TextView) view.FindViewById(Resource.Layout.Main);
 			MapView = view.FindViewById<MapView>(Resource.Id.mapView);
-			if(CurrentLocation != null)
-			if (!CurrentLocation.Text.Equals ("Your Location is:")) 
-			{
-				string[] coordinates = CurrentLocation.Text.Split (':');
-				foreach(var item in coordinates)
-				Log.Debug (Tag, item);
-			}
 			MapView.OnCreate(savedInstanceState);
 			return view;
 		}
@@ -59,15 +52,33 @@ namespace EmergencyApp_v2
 
 			if (Map != null)
 			{
-				Map.MyLocationEnabled = true;
+				Map.MyLocationEnabled = false;
+				MyLocation = null;
+				MyLastLocation = null;
+				Lat = new LatLng (45.469303, 9.188106);
+				MyLastLocation = MyLocation;
+				OnLocationChanged (Lat);
 
-				var latLng = new LatLng(45.478692D, 9.229734D);
+				float[] distance= new float[2];
+				float[] newDistance= new float[2];
 
-				var markerOptions = new MarkerOptions()
-					.SetPosition(latLng)
-					.Draggable(true);
-				NmtMarker = Map.AddMarker(markerOptions);
-				Map.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(latLng, 13));
+				Map.MyLocationChange += (s, e) => {
+
+					MyLastLocation = MyLocation;
+					MyLocation = e.Location;
+					if(MyLocation != null && MyLastLocation != null)
+					{
+						Location.DistanceBetween(MyLocation.Latitude,MyLastLocation.Latitude,MyLocation.Longitude,MyLocation.Latitude,distance);
+						Log.Debug (@"My Location is: ",distance.FirstOrDefault()+":"+distance.LastOrDefault());
+
+						if(Math.Abs(newDistance[0] - distance[0]) >= 1F && Math.Abs(newDistance[1] - distance[1]) >= 1F)
+						{	
+							Lat = new LatLng(MyLocation.Latitude, MyLocation.Longitude);
+							OnLocationChanged (Lat);
+							distance=newDistance;
+						}
+					}
+				};
 			}
 		}
 
@@ -80,13 +91,6 @@ namespace EmergencyApp_v2
 		public override void OnResume()
 		{
 			base.OnResume();
-			if(CurrentLocation != null)
-			if (!CurrentLocation.Text.Equals ("Your Location is:")) 
-			{
-				string[] coordinates = CurrentLocation.Text.Split (':');
-				foreach(var item in coordinates)
-					Log.Debug (Tag, item);
-			}
 			SetUpMapIfNeeded();
 			MapView.OnResume();
 		}
@@ -109,6 +113,29 @@ namespace EmergencyApp_v2
 			{
 				Map = View.FindViewById<MapView>(Resource.Id.mapView).Map;
 			}
+		}
+
+		public void OnLocationChanged (LatLng location)
+		{
+			if (NmtMarker != null) 
+			{
+				NmtMarker.Remove ();
+			}
+
+			Map.MyLocationEnabled = true;
+			MyLocation = null;
+			MyLastLocation = null;
+			Lat = location;
+			MyLastLocation = MyLocation;
+			var markerOptions = new MarkerOptions ()
+				.SetPosition (Lat)
+				.SetTitle ("You are here!")
+				.Draggable(true);
+
+			NmtMarker = Map.AddMarker(markerOptions);
+			Map.MoveCamera (CameraUpdateFactory.NewLatLngZoom(Lat, 10));
+			Map.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(Lat, 10));
+
 		}
 	}
 }
